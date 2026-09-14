9b0da76da9ed141dcd896cf5102fd8714d041b87
z-sulit <lorenzisolate@gmail.com>
2026-09-14 19:17:06 +0800

    dev

--- MIDTERM-backend/server.js
+++ MIDTERM-backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const users = require("./users");
 
 app.use(express.json()); // IMPORTANT: parse JSON body
 
@@ -7,4 +8,50 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+// post request
+app.post("/users", (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            message: "Name and email are required",
+        });
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name,
+        email,
+    };
+
+    users.push(newUser);
+
+    res.status(201).json(newUser);
+});
+
+// post request
+app.post("/users", (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            message: "Name and email are required",
+        });
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name,
+        email,
+    };
+
+    users.push(newUser);
+
+    res.status(201).json(newUser);
 });
