8b0ec6d785955eda533e4ca8e06fbe316afdd5a5
z-sulit <lorenzisolate@gmail.com>
2026-09-14 19:48:54 +0800

    ff

--- MIDTERM-backend/server.js
+++ MIDTERM-backend/server.js
@@ -13,7 +13,17 @@ app.listen(PORT, () => {
 app.get("/users", (req, res) => {
     res.json(users);
 });
+//getting a siungple user by id
+app.get("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
 
+    const user = users.find((u) => u.id === id);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+});
 // post request
 app.post("/users", (req, res) => {
     const { name, email } = req.body;
@@ -53,4 +63,34 @@ app.put("/users/:id", (req, res) => {
     };
 
     res.json(users[index]);
+});
+// patch (partial update/s)
+app.patch("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const user = users.find((u) => u.id === id);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    if (req.body.name !== undefined) {
+        user.name = req.body.name;
+    }
+
+    res.json(user);
+});
+// delete
+app.delete("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const index = users.findIndex((u) => u.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    users.splice(index, 1);
+
+    res.json({ message: "User deleted" });
 });
