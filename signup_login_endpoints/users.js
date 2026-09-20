f1837379f4e34a377109037e84fbae10f0a65eaa
z-sulit <lorenzisolate@gmail.com>
2026-09-20 17:20:20 +0800

    feat: initial commit signup_login_endpoints

---
+++ signup_login_endpoints/users.js
@@ -0,0 +1,10 @@
+// synthetic data for users
+
+let users = [
+    { id: 1, name: "John Doe", email: "john@example.com" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com" },
+    { id: 3, name: "sayuri", email: "sayuri@example.com" },
+    { id: 4, name: "Strelitzia", email: "strelitzia@example.com" }
+];
+
+module.exports = users;
