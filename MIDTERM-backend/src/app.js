42d99f68eb7d2d8a6b3990d16afb3fd2973becfe
z-sulit <lorenzisolate@gmail.com>
2026-09-21 19:34:31 +0800

    idk

--- MIDTERM-backend/src/app.js
+++ MIDTERM-backend/src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const userRoutes = require('./routes/user.routes');
+const userRoutes = require('./routes/users/routes.js');
 // const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(express.json());
-//app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 //app.use(errorHandler);
 
 module.exports = app;
--- MIDTERM-backend/src/routes/users/routes.js
+++ MIDTERM-backend/src/routes/users/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/users/controller'); // Note: The image contains a typo here ('requize' instead of 'require')
 
-router.get('/users', ctrl.getUsers);
+router.get('/', ctrl.getUsers);
 //router.post('/', ctrl.createUser);
 //router.put('/:id', ctrl.updateUser);
 //router.delete('/:id', ctrl.deleteUser);
